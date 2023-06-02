6d44d29d5f8c18e662f8dcf4d3130bd42bbeb1b9
macjonessoriano <sorianomacjones@gmail.com>
2023-06-02 23:24:25 +0800

    converting scss into styled-components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,40 @@
 import DirectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
 
-const Directory = ({ categories }) => {
+const categories = [
+	{
+		id: 1,
+		title: 'vape',
+		imageUrl: 'https://i.ibb.co/GQN5TLW/smok-nord-4.jpg',
+		route: 'shop/vape',
+	},
+	{
+		id: 2,
+		title: 'cap',
+		imageUrl: 'https://i.ibb.co/YkzZPq7/cap-sinio-3.jpg',
+		route: 'shop/cap',
+	},
+	{
+		id: 3,
+		title: 't-shirt',
+		imageUrl: 'https://i.ibb.co/16DNHxk/oversize.jpg',
+		route: 'shop/t-shirt',
+	},
+	{
+		id: 4,
+		title: 'sneakers',
+		imageUrl: 'https://i.ibb.co/h7XYyN2/shoes-4.jpg',
+		route: 'shop/sneakers',
+	},
+	{
+		id: 5,
+		title: 'smartphones',
+		imageUrl: 'https://i.ibb.co/DY1Sxjd/huawei-P50.jpg',
+		route: 'shop/smartphones',
+	},
+];
+
+const Directory = () => {
 	return (
 		<div className='directory-container'>
 			{categories.map((category) => {
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,37 +3,9 @@ import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
-	const categories = [
-		{
-			id: 1,
-			title: 'vape',
-			imageUrl: 'https://i.ibb.co/GQN5TLW/smok-nord-4.jpg',
-		},
-		{
-			id: 2,
-			title: 'cap',
-			imageUrl: 'https://i.ibb.co/YkzZPq7/cap-sinio-3.jpg',
-		},
-		{
-			id: 3,
-			title: 't-shirt',
-			imageUrl: 'https://i.ibb.co/16DNHxk/oversize.jpg',
-		},
-		{
-			id: 4,
-			title: 'sneakers',
-			imageUrl: 'https://i.ibb.co/h7XYyN2/shoes-4.jpg',
-		},
-		{
-			id: 5,
-			title: 'smartphones',
-			imageUrl: 'https://i.ibb.co/DY1Sxjd/huawei-P50.jpg',
-		},
-	];
-
 	return (
 		<div>
-			<Directory categories={categories} />
+			<Directory />
 			<Outlet />
 		</div>
 	);
