e64385bf12f6d18fb2ef452e473b953ae30eac82
macjonessoriano <sorianomacjones@gmail.com>
2023-06-04 12:15:04 +0800

    finalize the user reducer redux

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,15 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { useSelector } from 'react-redux';
+
 import NavLogo from '../../assets/mj-logo-primary-transparent.png';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
-
 import { signOutUser } from '../../utils/firebase/firebase.utils.js';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 import {
 	NavigationContainer,
@@ -19,7 +20,8 @@ import {
 } from './navigation.styles';
 
 const Navigation = () => {
-	const { currentUser } = useContext(UserContext);
+	// const { currentUser } = useContext(UserContext);
+	const currentUser = useSelector(selectCurrentUser);
 	const { isCartOpen } = useContext(CartContext);
 
 	return (
