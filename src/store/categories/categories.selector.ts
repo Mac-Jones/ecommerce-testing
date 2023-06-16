7a01f02d5e5136053e4bfacbb78cb080b90ddb1b
macjonessoriano <sorianomacjones@gmail.com>
2023-06-16 20:11:34 +0800

    save for now

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,6 +1,11 @@
 import { createSelector } from 'reselect';
 
-export const selectCategoryReducer = (state) => state.categories;
+import { CategoriesState } from './categories.reducer';
+
+import { CategoryMap } from './categories.types';
+
+export const selectCategoryReducer = (state): CategoriesState =>
+	state.categories;
 
 export const selectCategories = createSelector(
 	[selectCategoryReducer],
@@ -9,12 +14,12 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
 	[selectCategories],
-	(categories) => {
+	(categories): CategoryMap => {
 		return categories.reduce((acc, category) => {
 			const { title, items } = category;
 			acc[title.toLowerCase()] = items;
 			return acc;
-		}, {});
+		}, {} as CategoryMap);
 	}
 );
 
